42f53a21cd7b812ffaf56ed96b4216aeb71c36c8
GinantiRiski1 <ginantiriski66@gmail.com>
2025-05-10 22:58:37 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,17 +1,17 @@
 import pandas as pd
 import mlflow
 import mlflow.sklearn
-import argparse
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
+import argparse
 
-# Argumen
-parser = argparse.ArgumentParser()
-parser.add_argument('--n_neighbors', type=int, default=3)
-args = parser.parse_args()
-
-# Autolog MLflow
+# Autolog harus diaktifkan sebelum training
 mlflow.sklearn.autolog()
+
+# Argumen dari command line (parameter MLflow Project)
+parser = argparse.ArgumentParser()
+parser.add_argument("--n_neighbors", type=int, default=3)
+args = parser.parse_args()
 
 # Load data
 X_train = pd.read_csv("car_preprocessing/X_train.csv")
@@ -19,13 +19,11 @@ X_test = pd.read_csv("car_preprocessing/X_test.csv")
 y_train = pd.read_csv("car_preprocessing/y_train.csv").values.ravel()
 y_test = pd.read_csv("car_preprocessing/y_test.csv").values.ravel()
 
-# Set experiment
-mlflow.set_experiment("basic-model_v2")
+# Inisialisasi dan latih model
+model = KNeighborsClassifier(n_neighbors=args.n_neighbors)
+model.fit(X_train, y_train)
 
-with mlflow.start_run():
-    model = KNeighborsClassifier(n_neighbors=args.n_neighbors)
-    model.fit(X_train, y_train)
-
-    preds = model.predict(X_test)
-    acc = accuracy_score(y_test, preds)
-    print(f"Accuracy: {acc}")
+# Prediksi dan evaluasi
+preds = model.predict(X_test)
+acc = accuracy_score(y_test, preds)
+print(f"Accuracy: {acc}")
