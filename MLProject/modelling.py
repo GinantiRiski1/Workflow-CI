743cfa448eca2aaa00956472e420cc17f15fdac8
GinantiRiski1 <ginantiriski66@gmail.com>
2025-05-10 16:45:06 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -2,36 +2,38 @@ import argparse
 import pandas as pd
 import mlflow
 import mlflow.sklearn
+import joblib
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 
-# Argument parser untuk menerima parameter dari MLproject
-parser = argparse.ArgumentParser()
-parser.add_argument("--n_neighbors", type=int, default=3)
-args = parser.parse_args()
+def load_data():
+    X_train = pd.read_csv("car_preprocessing/X_train.csv")
+    X_test = pd.read_csv("car_preprocessing/X_test.csv")
+    y_train = pd.read_csv("car_preprocessing/y_train.csv").values.ravel()
+    y_test = pd.read_csv("car_preprocessing/y_test.csv").values.ravel()
+    return X_train, X_test, y_train, y_test
 
-# Autolog harus diaktifkan sebelum training
-mlflow.sklearn.autolog()
+def train_model(n_neighbors):
+    mlflow.set_experiment("basic-model_v2")
+    mlflow.sklearn.autolog()
 
-# Load data
-X_train = pd.read_csv("car_preprocessing/X_train.csv")
-X_test = pd.read_csv("car_preprocessing/X_test.csv")
-y_train = pd.read_csv("car_preprocessing/y_train.csv").values.ravel()
-y_test = pd.read_csv("car_preprocessing/y_test.csv").values.ravel()
+    X_train, X_test, y_train, y_test = load_data()
 
-# Set nama eksperimen
-mlflow.set_experiment("basic-model_v2")
+    with mlflow.start_run():
+        model = KNeighborsClassifier(n_neighbors=n_neighbors)
+        model.fit(X_train, y_train)
 
-# Mulai MLflow run
-with mlflow.start_run():
-    model = KNeighborsClassifier(n_neighbors=args.n_neighbors)
-    model.fit(X_train, y_train)
+        preds = model.predict(X_test)
+        acc = accuracy_score(y_test, preds)
+        print(f"Accuracy: {acc}")
 
-    # Prediksi dan akurasi
-    preds = model.predict(X_test)
-    acc = accuracy_score(y_test, preds)
-    print(f"Accuracy: {acc}")
+        # Simpan model ke file
+        joblib.dump(model, "trained_model.pkl")
+        mlflow.log_artifact("trained_model.pkl")
 
-    # Log manual (jika autolog tidak digunakan)
-    mlflow.sklearn.log_model(model, "model")
-    mlflow.log_metric("accuracy", acc)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--n_neighbors", type=int, default=3, help="Jumlah tetangga untuk KNN")
+    args = parser.parse_args()
+
+    train_model(args.n_neighbors)
