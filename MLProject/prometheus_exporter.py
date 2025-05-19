1fda68f8bdaf8e1d5b69b3b85c69e969c8c8ac98
GinantiRiski1 <ginantiriski66@gmail.com>
2025-05-19 16:52:14 +0700

    Update prometheus_exporter.py

--- MLProject/prometheus_exporter.py
+++ MLProject/prometheus_exporter.py
@@ -1,9 +1,11 @@
-from prometheus_client import Counter, Histogram, generate_latest
+from prometheus_client import Counter, Histogram, generate_latest, CONTENT_TYPE_LATEST
 from flask import Response
 
+# Metrik Prometheus yang digunakan
 REQUEST_COUNT = Counter("request_count", "Total number of requests")
-INFERENCE_TIME = Histogram("inference_time_seconds", "Time spent on prediction")
+REQUEST_ERRORS = Counter("request_errors", "Total number of errors")
+INFERENCE_TIME = Histogram("inference_time_seconds", "Time spent on inference")
 
-@app.route("/metrics")
-def metrics():
-    return Response(generate_latest(), mimetype="text/plain")
+# Endpoint metrics
+def prometheus_metrics():
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
